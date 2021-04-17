6d1a7edcc11ab4919f4e73fe986b4beb22d16a18
Matthew Wong <wongmat@oregonstate.edu>
2021-04-17 09:48:18 -0700

    this should work for bottom up

--- Assignment3/Game.py
+++ Assignment3/Game.py
@@ -3,33 +3,38 @@ import os
 import random
 import re
 import sys
+#global variable
+g = 0
 
 #bottom up approach
 def game_bottomup(n):
+    #global g
+    #g = g + 1
+    #if (g % 2 == 1):
+    #    print("its A's turn")
+    #else:
+    #    print("its B's turn")
     #base cases
-    print("bottom up start: n val = ", n)
+    #print("bottom up start: n val = ", n)           #TEST
     if (n == 2):                                    #insta win for A
         return True
 
     if (n == 3):                                    #insta win for B no winnable cases for A
         return False
 
-    #x = 0                                          #storage
     #loop code to find factors
-    #for i in range(1, math.floor(n/2)):
     for i in range(1, n):
-        print("i = ", i)
+        #print("i = ", i)                            #TEST
         if n % i == 0:                              #we find the factor
             lastfactor = i
-            print("i is factor", i)                 #should print the factor
+            #print("i is factor", i)                 #TEST:should print the factor
             if (((n-i) % 2) == 1):                  #check if its odd to force odd to next person
-                #x = i
-                print("if ODD state", i)
+                #print("if ODD state", i)            #TEST ODD state
                 return(game_bottomup(n-i))
-            else:
-                print("else EVEN state", i)
-    print("exit i val ", i)
-    print("Lastfact ", lastfactor)
+            #else:
+                #print("else EVEN state", i)         #TEST EVEN state
+    #print("exit i val ", i)                         #TEST exit val
+    #print("Lastfact ", lastfactor)                  #TEST lastfactor val
     return(game_bottomup(n-lastfactor))
 
 
@@ -54,5 +59,5 @@ def game_bottomup(n):
 
 #top down approach
 #def game_topdown(N):
-print(game_bottomup(9))    #should return true
+print(game_bottomup(3))    #should return true
 #print(game_bottomup(3))    #should return false
