118898206532bc2952d6e0281e1bd739b9eff885
Matthew Wong <wongmat@oregonstate.edu>
2021-04-22 16:07:17 -0700

    finished the first part of the assignment cited: https://www.geeksforgeeks.org/maximum-sum-such-that-no-two-elements-are-adjacent/

---
+++ Assignment4/MaxSet.py
@@ -0,0 +1,17 @@
+def max_indepedent_set(arr):
+    including = 0                               #used for calculating including
+    excluding = 0                               #used for calculating excluding
+
+    for i in arr:
+        #calculate max possible excluding i
+        temp = excluding if excluding > including else including                #grabs the largest always and stores in temp
+
+        #accumulator variable to calculate max including i
+        including = excluding + i                                               #including now is exluding + i
+        excluding = temp                                                        #excluding now is set to temp
+
+    #return back the maximum possible
+    return (excluding if excluding > including else including)                  #sends back maximum possible
+
+arr = [7, 2, 5, 8, 6]
+print max_indepedent_set(arr)
