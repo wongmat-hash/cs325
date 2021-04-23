37290f0e91c3d229fa34864933ab57c8fd179325
Matthew Wong <wongmat@oregonstate.edu>
2021-04-22 22:44:53 -0700

    commit

--- Assignment4/MaxSet.py
+++ Assignment4/MaxSet.py
@@ -5,11 +5,12 @@ def max_indepedent_set(arr):
     for i in arr:
         #calculate max possible excluding i
         temp = excluding if excluding > including else including                #grabs the largest always and stores in temp
-
+        #print("temp", temp)
         #accumulator variable to calculate max including i
         including = excluding + i                                               #including now is exluding + i
+        #print("including", including)
         excluding = temp                                                        #excluding now is set to temp
-
+        #print("excluding", excluding)
     #return back the maximum possible
     return (excluding if excluding > including else including)                  #sends back maximum possible
 
