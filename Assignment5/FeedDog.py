6d8156b53f8b064e2c558620a607793e56dbfe87
Matthew Wong <wongmat@oregonstate.edu>
2021-04-29 20:59:40 -0700

    part 2 is done

--- Assignment5/FeedDog.py
+++ Assignment5/FeedDog.py
@@ -1,22 +1,19 @@
-def feedDog(hunger_index,biscuit_size):
-        hunger_index.sort()
-        biscuit_size.sort()
-        count=0
+def feedDog(hunger_index,biscuit_size):                                         #takes in two arrays hunger level and biscuit size
+        hunger_index.sort()                                                     #first part of greedy is to apply a sort
+        biscuit_size.sort()                                                     #first part of greedy is to apply a sort so we sort both
+        dog_count=0                                                             #init a count value
         j=0
-        for i in biscuit_size:
-                for k in range(j,len(hunger_index)):
-                        if(i>=hunger_index[k]):
-                                count+=1
-                                j=j+1
+        for i in biscuit_size:                                                  #use two loops to loop through our double arrays start with BS
+                for k in range(j,len(hunger_index)):                            #go from 0 to the size of hunger index
+                        if(i>=hunger_index[k]):                                 #if the number of biscuits is greater or = to the hunger index array k
+                                dog_count+=1                                    #we add to our count
+                                j=j+1                                           #increment our j value
                                 break
-                if(j==len(hunger_index)):
-                        return count
+                if(j==len(hunger_index)):                                       #if j ends up at the hunger index
+                        return count                                            #we've hit the end and return count
+        return count                                                            #return count
 
-        return count
-# for user input uncomment below 2 lines and comment 3rd and 4 th line after this line
-# hunger_index = [ int(x) for x in input().split()]
-# biscuit_size = [ int(x) for x in input().split()]
 hunger_index =[1,2]
 biscuit_size =[1,2,3]
-No_DogSatisfy = feedDog(hunger_index,biscuit_size)
-print(No_DogSatisfy)
+doggyHunger = feedDog(hunger_index,biscuit_size)
+print(doggyHunger)
