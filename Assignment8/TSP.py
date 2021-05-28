0541bb89a483f16687bf51b3c0acbb8ab00c6916
Matthew Wong <wongmat@oregonstate.edu>
2021-05-28 13:09:02 -0700

    udpated comments

--- Assignment8/TSP.py
+++ Assignment8/TSP.py
@@ -2,40 +2,33 @@ from sys import maxsize
 from itertools import permutations
 V = 4
 
-# implementation of traveling Salesman Problem
+#TSP with MST 
 def approx_tsp_algo(G):
     s = 0
-    # store all vertex apart from source vertex
-    vertex = []
+    vertex = []                                                 #store vertex from source
     for i in range(V):
         if i != s:
             vertex.append(i)
 
-    # store minimum weight Hamiltonian Cycle
-    min_path = maxsize
+    min_path = maxsize                                          #hamiltonian cycle w
     next_permutation=permutations(vertex)
     for i in next_permutation:
 
-        # store current Path weight(cost)
-        current_pathweight = 0
+        current_pathweight = 0                                  #store path
 
-        # compute current path weight
         k = s
-        for j in i:
+        for j in i:                                             #calculate path W
             current_pathweight += G[k][j]
             k = j
         current_pathweight += G[k][s]
 
-        # update minimum
-        min_path = min(min_path, current_pathweight)
+        min_path = min(min_path, current_pathweight)            #update min
 
     return min_path
 
 
-# Driver Code
 if __name__ == "__main__":
 
-    # matrix representation of G
     G = [[0, 4, 3, 1], [4, 0, 1, 2],
             [3, 1, 0, 5], [1, 2, 5, 0]]
     print(approx_tsp_algo(G))
