1e524eee21f06c845270b52cdff201664b84bf54
Matthew Wong <wongmat@oregonstate.edu>
2021-06-03 20:02:22 -0700

    complete problem B

--- Assignment9/Patternmatch.py
+++ Assignment9/Patternmatch.py
@@ -1,78 +1,31 @@
-# Python program to implement wildcard
-# pattern matching algorithm
+def patternmatch(string, p):                                                    #function that takes in string and pattern
+    s_len = len(string)                                                         #set the string length to s_len
+    if len(p) - p.count('*') > s_len:                                           #check if the length of p is less than s_len
+        return False                                                            #already impossible for a pattern match
+    dp = [True] + [False]*s_len                                                 #make an array to store our comparison
+    for i in p:                                                                 #loop through to p
+        if i != '*':                                                            #try to find '*'
+            for n in reversed(range(s_len)):                                    #look through the reversed of s
+                dp[n+1] = dp[n] and (i == string[n] or i == '?')                #check for ?
+        else:
+            for n in range(1, s_len+1):                                         #otherwise loop again to s len
+                dp[n] = dp[n-1] or dp[n]                                        #set the array we use for storage
+        dp[0] = dp[0] and i == '*'                                              #set the pattern '*'
+    return dp[-1]
 
-# Function that matches input strr with
-# given wildcard pattern
+#test cases 
+string = "abcde"
+pattern = '*'
+print(patternmatch(string, pattern))
 
+string2 = 'abcde'
+pattern2 = '*a?c*'
+print(patternmatch(string2, pattern2))
 
-def strrmatch(strr, pattern, n, m):
+string3 = 'abcde'
+pattern3 = 'ad'
+print(patternmatch(string3, pattern3))
 
-    # empty pattern can only match with
-    # empty strring
-    if (m == 0):
-        return (n == 0)
-
-    # lookup table for storing results of
-    # subproblems
-    lookup = [[False for i in range(m + 1)] for j in range(n + 1)]
-
-    # empty pattern can match with empty strring
-    lookup[0][0] = True
-
-    # Only '*' can match with empty strring
-    for j in range(1, m + 1):
-        if (pattern[j - 1] == '*'):
-            lookup[0][j] = lookup[0][j - 1]
-
-    # fill the table in bottom-up fashion
-    for i in range(1, n + 1):
-        for j in range(1, m + 1):
-
-            # Two cases if we see a '*'
-            # a) We ignore ‘*’ character and move
-            # to next character in the pattern,
-            # i.e., ‘*’ indicates an empty sequence.
-            # b) '*' character matches with ith
-            # character in input
-            if (pattern[j - 1] == '*'):
-                lookup[i][j] = lookup[i][j - 1] or lookup[i - 1][j]
-
-            # Current characters are considered as
-            # matching in two cases
-            # (a) current character of pattern is '?'
-            # (b) characters actually match
-            elif (pattern[j - 1] == '?' or strr[i - 1] == pattern[j - 1]):
-                lookup[i][j] = lookup[i - 1][j - 1]
-
-            # If characters don't match
-            else:
-                lookup[i][j] = False
-
-    return lookup[n][m]
-
-# Driver code
-
-
-strr = "baaabab"
-pattern = "*****ba*****ab"
-# char pattern[] = "ba*****ab"
-# char pattern[] = "ba*ab"
-# char pattern[] = "a*ab"
-# char pattern[] = "a*****ab"
-# char pattern[] = "*a*****ab"
-# char pattern[] = "ba*ab****"
-# char pattern[] = "****"
-# char pattern[] = "*"
-# char pattern[] = "aa?ab"
-# char pattern[] = "b*b"
-# char pattern[] = "a*a"
-# char pattern[] = "baaabab"
-# char pattern[] = "?baaabab"
-# char pattern[] = "*baaaba*"
-
-if (strrmatch(strr, pattern, len(strr), len(pattern))):
-    print("Yes")
-else:
-    print("No")
-
-# This code is contributed by shubhamsingh10
+string4 = 'abcde'
+pattern4 = 'ad?'
+print(patternmatch(string4, pattern4))
